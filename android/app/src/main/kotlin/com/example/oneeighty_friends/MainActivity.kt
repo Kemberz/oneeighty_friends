a4ea1f98233906f0ad04f41abcb9bab410951424
Kemberz <nuno.silva.campos@gmail.com>
2024-06-20 11:11:44 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/oneeighty_friends/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.oneeighty_friends
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Firebase Setup'),
+      ),
+      body: Center(
+        child: Text('Firebase is initialized!'),
+      ),
+    );
+  }
+}
